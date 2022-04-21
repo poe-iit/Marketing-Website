5bb96a9e831395e20b386a6c9facd94c5decbbca
chum-o <sammymichelsen@gmail.com>
2022-04-21 14:11:22 -0500

    added wiki content

---
+++ wiki.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("wiki-container").style.marginLeft = "280px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("wiki-container").style.marginLeft = "100px";
+}
